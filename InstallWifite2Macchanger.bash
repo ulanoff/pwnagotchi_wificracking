41c65a468f8e99a5becb7b90c1f212baee108222
ulanoff <109368484+ulanoff@users.noreply.github.com>
2022-07-16 09:16:54 +0300

    Add files via upload

---
+++ InstallWifite2Macchanger.bash
@@ -0,0 +1,56 @@
+#!/bin/bash
+cd /home/pi/
+sudo apt install macchanger
+macchanger -m DE:AD:BE:EF:DE:AD mon0
+macchanger -s mon0
+
+sudo apt-get install aircrack-ng
+sudo apt-get install reaver
+sudo apt install tshark
+
+git clone https://github.com/derv82/wifite2.git
+cd wifite2/
+#To run wifite2 without installation -> sudo ./Wifite.py
+
+cd /home/pi/
+#LIBRARIES
+sudo apt-get install libcurl4-openssl-dev libssl-dev pkg-config
+
+#HCXDUMPTOOL
+git clone https://github.com/ZerBea/hcxdumptool.git
+cd hcxdumptool/
+make
+make install
+
+#HCXTOOLS
+cd /home/pi/
+git clone https://github.com/ZerBea/hcxtools.git
+cd hcxtools/
+make 
+make install
+
+#TRYING CREATING LINK
+ln -s /usr/local/bin/hcxpcapngtool /usr/local/bin/hcxpcaptool #NOT WORKS
+
+#HCXPCAPTOOL INSTALLATION (IF LINK DOESNT WORK)
+cd /home/pi/
+git clone https://github.com/warecrer/Hcxpcaptool
+cd Hcxpcaptool/
+make
+make install
+
+#BULLY
+cd /home/pi/
+git clone https://github.com/aanarchyy/bully
+cd bully/src/
+make
+sudo make install
+
+#PIXIEWPS
+cd /home/pi/
+git clone https://github.com/wiire-a/pixiewps
+cd pixiewps/
+make
+sudo make install
+
+#### HASHCAT AND PYRIT INSTALLATION HAS BROKEN MY OS
